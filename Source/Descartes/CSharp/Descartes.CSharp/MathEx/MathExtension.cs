830c49bf87ac8a0c9e7d99a8d60aded2c6b60c79
akihiro takada <umebayashi.akihiro@gmail.com>
2012-08-09 01:33:25 +0900

    名前空間変更(Umebayashi => Descartes)

---
+++ Source/Descartes/CSharp/Descartes.CSharp/MathEx/MathExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Descartes.MathEx
+{
+	public static class MathExtension
+	{
+		public static int Factorial(this int n)
+		{
+			if (n == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return n * Factorial(n - 1);
+			}
+		}
+
+		public static long Factorial(this long n)
+		{
+			if (n == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				return n * Factorial(n - 1);
+			}
+		}
+	}
+}
